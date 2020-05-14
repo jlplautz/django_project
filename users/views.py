061c4b1e94c36852bce6b3b6912c65ecc39a527f
61246880 <jorge.plautz@gmail.com>
2020-05-14 18:00:32 -0300

    Corrected README file
     close #29

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -19,11 +20,13 @@ def register(request):
             # There are different types of messages
             # messages.debug    # messages.info     # messages.success
             # messages.warning  # messages.error
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
-
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
